bedf199f152cba664bcae02f0e1cda32bc1d8817
kunal999 <kunal999@gmail.com>
2017-09-27 17:23:35 -0400

    fix(geolocation): remove $rootScope.$apply on error
    re: https://app.assembla.com/spaces/woodspring-hotels/tickets/realtime_list?ticket=666

--- src/geolocation.js
+++ src/geolocation.js
@@ -16,22 +16,20 @@ angular.module('geolocation')
           $window.navigator.geolocation.getCurrentPosition(function(position){
             $rootScope.$apply(function(){deferred.resolve(position);});
           }, function(error) {
-            $rootScope.$apply(function() {
-              switch (error.code) {
-                case 1:
-                  $rootScope.$broadcast('error',geolocation_msgs['errors.location.permissionDenied']);
-                  deferred.reject(geolocation_msgs['errors.location.permissionDenied']);
-                  break;
-                case 2:
-                  $rootScope.$broadcast('error',geolocation_msgs['errors.location.positionUnavailable']);
-                  deferred.reject(geolocation_msgs['errors.location.positionUnavailable']);
-                  break;
-                case 3:
-                  $rootScope.$broadcast('error',geolocation_msgs['errors.location.timeout']);
-                  deferred.reject(geolocation_msgs['errors.location.timeout']);
-                  break;
-              }              
-            });
+            switch (error.code) {
+              case 1:
+                $rootScope.$broadcast('error',geolocation_msgs['errors.location.permissionDenied']);
+                deferred.reject(geolocation_msgs['errors.location.permissionDenied']);
+                break;
+              case 2:
+                $rootScope.$broadcast('error',geolocation_msgs['errors.location.positionUnavailable']);
+                deferred.reject(geolocation_msgs['errors.location.positionUnavailable']);
+                break;
+              case 3:
+                $rootScope.$broadcast('error',geolocation_msgs['errors.location.timeout']);
+                deferred.reject(geolocation_msgs['errors.location.timeout']);
+                break;
+            }              
           }, opts);
         }
         else
